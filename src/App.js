ad7d775765f7b371ce3ed245f2d388b7fc14398b
ilus1 <ilusdasilva@gmail.com>
2023-05-12 11:06:47 -0300

    Componente de Titulo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Title';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Title title="Altura máxima" />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Title/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import defaultColors from '../../settings/defaultSettings'
+
+export const Container = styled.div`
+  font-family: 'Roboto';
+  font-size: 24px;
+  font-weight: 700;
+  color: ${defaultColors.primary};
+`
