d43befc9b61962e8a89ab52778c9e425e247f4bd
LucasGlopes <lucasglpes@gmail.com>
2023-07-06 16:11:39 -0300

    Testes de rota da tela de nova pintura

---
+++ src/__tests__/__pages__/NewPainting.test.tsx
@@ -0,0 +1,29 @@
+import { expect, beforeEach, describe, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import NewPainting from '../../pages/NewPainting';
+import { UserProvider } from '../../contexts/UserContext';
+
+describe('Teste da tela Nova Pintura', () => {
+    beforeEach(() => {
+        render(<UserProvider><NewPainting /></UserProvider>, {wrapper: BrowserRouter})    
+    });
+
+    it('Navegação para tela de pintura em andamento', async () => {    
+        const paintOptions = [
+            { label: 'Vermelho', value: 'Vermelho' },
+            { label: 'Azul', value: 'Azul' },
+            { label: 'Verde', value: 'Verde' },
+          ];
+        
+        const user = userEvent.setup()
+        const newPaintingButton = screen.getByText('Iniciar Pintura')
+    
+        await user.click(newPaintingButton)
+        expect(window.location.pathname).toBe('/ongoing-painting');
+    });
+
+})
+
