03de667d65b11eabf25b0289c204b4e15bf4ce54
LucasGlopes <lucasglpes@gmail.com>
2023-07-12 11:43:28 -0300

    Altera ícone de conexão

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import { Container, RobotPainting, MainTitle } from "./styles";
 import ConnectionStatus from "../ConnectionStatus";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { MqttContext } from "../../contexts/MqttContext";
 
 const Header = () => {
   const navigate = useNavigate();
+  const {isConnected} = useContext(MqttContext);
 
   return (
     <Container>
       <RobotPainting />
       <MainTitle onClick={() => navigate('/')}>{'Robô Pintor'}</MainTitle>
-      <ConnectionStatus isConnected={false} />
+      <ConnectionStatus isConnected={isConnected} />
     </Container>
   )
 }
